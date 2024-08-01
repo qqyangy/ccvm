72026fcd1a774a4d62ca86c619fd1fa18c1b37a0
yangyong <yangyong@pitayagames.com>
2024-08-01 10:37:04 +0800

    查找vm判断初始化状态标准调整

--- modules/BindBase.ts
+++ modules/BindBase.ts
@@ -267,10 +267,12 @@ export class BindBase extends Component {
     public getVmNodeComponent(): VmComponent {
         if (!this.node) return null;
         let parent: Node = this.node.parent;
-        while (parent && parent instanceof Node && parent.components?.length > 0) {
-            const vms = this.getVmComponent(parent),
-                vm = vms.find((_vm: VmComponent) => _vm.isVmNode);
-            if (vm) return vm;
+        while (parent && parent instanceof Node && parent.components && parent.children?.length > 0) {
+            if (parent.components?.length > 0) {
+                const vms = this.getVmComponent(parent),
+                    vm = vms.find((_vm: VmComponent) => _vm.isVmNode);
+                if (vm) return vm;
+            }
             parent = parent.parent;
         }
         return null;
